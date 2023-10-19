7da7264a7c89abe51c0e38eb5056d92b00d125fd
Syed Wasim Ahmed <iamwasimreal28@gmail.com>
2023-10-19 17:37:30 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import FunctionContextComponent from './FunctionContextComponent'
+import ClassContextComponent from './ClassContextComponent'
 
-function App() {
+export const ThemeContext = React.createContext()
+
+export default function App(){
+  const [darkTheme, setDarkTheme] = useState(true)
+
+  function toggleTheme(){
+    setDarkTheme(prevDarkTheme => !prevDarkTheme)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    < ThemeContext.Provider value={darkTheme}>
+     <button onClick={toggleTheme}>Toggle Theme</button>
+     <FunctionContextComponent/>
+     <ClassContextComponent/>
+    </ThemeContext.Provider></>
   );
-}
-
-export default App;
+  }
